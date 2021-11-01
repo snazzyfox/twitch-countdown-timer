1ddfaabe77d4a94f78ff6536ee412975a1d670af
SnazzyFox <12706268+snazzyfox@users.noreply.github.com>
2021-10-31 20:49:56 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Twitch Countdown Timer
+
+A countdown timer for Twitch that can be used by mods to run quick countdowns on stream.
+
+## How to use
+
+### Setting up stream
+
+In your streaming software, create a new browser source with the following parameters: 
+
+- URL: [link here]
+- Width: 640, Height 240
+- Shutdown source when not active: if selected, the timer will not receive any commands when it's hidden in your stream.
+
+The first time you load the page, you will need to enter interact mode for your browser source, and enter your channel name and click "save". This will be remembered for future uses. 
+
+If you entered the wrong channel name, double click on the page to clear saved data.
+
+### Usage
+
+| Command | Example | Description |
+| --------------------------- | ---------------------- | ---------------------------------------------------------------|
+| `!timer [duration]`         | `!timer 5m`            | Start a new timer with the given duration with no title. |
+| `!timer [duration] [title]` | `!timer 5m Prediction` | Start a new timer with the given title, which will show above the timer. |
+| `!timer` | `!timer` | Clears the current timer so it disappears. When the timer reaches zero, it'll stay on the screen until cleared. |
+
+This tool does not have a bot - the entire thing runs in your browser. It will not receive commands when the page is closed / your stream source is shutdown. However, it will remember the last active timer, and will continue counting even if the page is shut down.
---
+++ countdown.js
@@ -0,0 +1,85 @@
+var endTime, title;
+
+function getChannelName() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('channel');
+}
+
+function padNumber(num) {
+    return num.toString().padStart(2, "0");
+}
+
+function updateTimer() {
+    const now = Date.now();
+    const el = document.getElementById("timer");
+    if (endTime) {
+        const duration = endTime - now;
+        if (duration > 0) {
+            const days = Math.floor(duration / (24 * 60 * 60 * 1000));
+            const hours = Math.floor(duration / (60 * 60 * 1000) % 24);
+            const minutes = Math.floor(duration / (60 * 1000) % 60);
+            const seconds = Math.floor(duration / (1000) % 60);
+            var formatted = ""
+            if (days > 0) {
+                formatted += days.toString() + ":";
+            }
+            if (hours > 0) {
+                formatted += padNumber(days.toString()) + ":";
+            }
+            formatted += padNumber(minutes) + ":" + padNumber(seconds);
+            el.classList.remove("done");
+        } else {
+            formatted = "00:00";
+            el.classList.add("done");
+        }
+        document.getElementById("timer").innerText = formatted;
+        document.getElementById("title").innerText = title || '';
+    }
+}
+
+ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
+    if ( (flags.moderator || flags.broadcaster) && command == 'timer' ) {
+        var input_duation;
+        [input_duration, title] = message.split(/\s+/, 2);
+        var duration;
+        if (!input_duration) {
+            duration = undefined;
+        } else {
+            if (!isNaN(Number(input_duration))) input_duration = input_duration + "s"; // default to seconds if no units
+            duration = Duration.parse(input_duration).milliseconds();
+        }
+        console.log("New timer", duration, "title", title);
+        endTime = Date.now() + duration;
+        localStorage.setItem("endTime", endTime);
+        if (title) {
+            localStorage.setItem("title", title);
+        } else {
+            localStorage.removeItem("title");
+        }
+    }
+}
+
+window.addEventListener('load', () => {
+    // Hide channel name input if channel name is already set
+    const channelName = localStorage.getItem("channel");
+    endTime = parseInt(localStorage.getItem("endTime"));
+    title = localStorage.getItem("title");
+    if (channelName) {
+        ComfyJS.Init(channelName);
+        document.getElementById("channel-form").style.display = 'none';
+    }
+    setInterval(updateTimer, 500);
+})
+
+document.getElementById("channel-go").addEventListener("click", () => {
+    const newValue = document.getElementById("channel-input").value;
+    if (newValue) {
+        localStorage.setItem("channel", newValue);
+        window.location.reload();
+    }
+})
+
+window.addEventListener('dblclick', () => {
+    localStorage.clear();
+    window.location.reload();
+})
